918b8bed785cdca466a842d7886c827187293973
dodichakill <dodiherlangga91@gmail.com>
2023-11-03 12:57:45 +0700

    add: UI login

---
+++ 22090036-dodi/src/main/java/com/mycompany/dodi/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dodi;
+
+/**
+ *
+ * @author dodi
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
